006918e7f1e11935efa1ec1430d7b7309232c7f0
Mark Christian <m@rkchristian.ca>
2022-12-05 13:36:24 -0800

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use dotenv::dotenv;
+
 fn main() {
-    println!("Hello, world!");
+    let did_find_dotenv = dotenv().ok().is_some();
+    if cfg!(debug_assertions) && !did_find_dotenv {
+        println!("Debug-only warning: no .env file found to configure logging; all logging will be disabled. Add RUST_LOG=info to .env to see logging.");
+    }
+    env_logger::init();
+
+    log::info!("Hello, world!");
 }
