193e17501732871a2b17cbe976fae6d507edf880
Mark Christian <m@rkchristian.ca>
2022-12-05 13:49:51 -0800

    Add anyhow

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@
 
 use dotenv::dotenv;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let did_find_dotenv = dotenv().ok().is_some();
     if cfg!(debug_assertions) && !did_find_dotenv {
         println!("Debug-only warning: no .env file found to configure logging; all logging will be disabled. Add RUST_LOG=info to .env to see logging.");
@@ -17,4 +17,6 @@ fn main() {
     env_logger::init();
 
     log::info!("Hello, world!");
+
+    Ok(())
 }
