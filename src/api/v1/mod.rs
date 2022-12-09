f96bd7bee4105f38c31b8a5464caef2477aa7467
Mark Christian <m@rkchristian.ca>
2022-12-09 10:57:02 -0800

    Refactor JobQueue to avoid global state
    Rather than using a global singleton Vec<Job> as the job queue with a bunch of functions that
    manipulate the global singleton, this makes a JobQueue struct and moves all of the functions
    onto it.
    
    Having done that, the JobQueue ended up fitting into Axum's state nicely. This is much better
    in prett much every dimension and is fairly obvious in retrospect; I am not sure why I did it
    wrong the first time around. 😅

--- src/api/v1/mod.rs
+++ src/api/v1/mod.rs
@@ -1,13 +1,16 @@
+use std::sync::Arc;
+
 use axum::{
-    extract::{Json, Path},
+    extract::{Json, Path, State},
     http::StatusCode,
     response::{IntoResponse, Response},
 };
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
+use tokio::sync::Mutex;
 use uuid::Uuid;
 
-use crate::queue::{claim_job, complete_job, enqueue_job, fail_job, get_job, tickle_job, Job};
+use crate::queue::{Job, JobQueue};
 
 // follow this pattern for endpoint groups
 // pub mod <filename>;
@@ -28,17 +31,25 @@ pub struct JobsCreateResponse {
     job_id: String,
 }
 
+#[derive(Clone)]
+pub struct AxumState {
+    pub job_queue: Arc<Mutex<JobQueue>>,
+}
+
 type JsonHandlerResult<T> = Result<Json<T>, Response>;
 
 pub async fn create(
+    State(state): State<AxumState>,
     Json(payload): Json<JobsCreateRequest>,
 ) -> JsonHandlerResult<JobsCreateResponse> {
+    let mut job_queue = state.job_queue.lock().await;
+
     // TODO: validate that payload.binary_addr and payload.input_addr are valid-looking addresses.
     // Ideally this would happen at the Serde level but I don't see a way to implement constraints
     // on string fields. My best idea so far would be to make these fields into [u8;40] and tell
     // Serde to represent them as hex strings when in JSON format. This seems not ideal.
     log::info!("Enqueueing job {payload:?}");
-    let Ok(job_id) = enqueue_job(payload.binary_addr, payload.input_addr) else {
+    let Ok(job_id) = job_queue.enqueue_job(payload.binary_addr, payload.input_addr) else {
         return Err((StatusCode::INTERNAL_SERVER_ERROR, String::from("Failed to enqueue job")).into_response());
     };
 
@@ -52,16 +63,28 @@ pub struct JobsClaimRequest {
     runner_id: Uuid,
 }
 
-pub async fn claim(Json(payload): Json<JobsClaimRequest>) -> JsonHandlerResult<Job> {
-    let Some(job) = claim_job(&payload.runner_id) else {
+pub async fn claim(
+    State(state): State<AxumState>,
+    Json(payload): Json<JobsClaimRequest>,
+) -> JsonHandlerResult<Job> {
+    let mut job_queue = state.job_queue.lock().await;
+    job_queue.detect_abandoned_jobs();
+
+    let Some(job) = job_queue.claim_job(&payload.runner_id) else {
         return Err((StatusCode::NO_CONTENT, String::from("No pending jobs")).into_response());
     };
 
     Ok(Json(job))
 }
 
-pub async fn tickle(Path(job_id): Path<Uuid>) -> JsonHandlerResult<Value> {
-    let Ok(()) = tickle_job(&job_id) else {
+pub async fn tickle(
+    Path(job_id): Path<Uuid>,
+    State(state): State<AxumState>,
+) -> JsonHandlerResult<Value> {
+    let mut job_queue = state.job_queue.lock().await;
+    job_queue.detect_abandoned_jobs();
+
+    let Ok(()) = job_queue.tickle_job(&job_id) else {
         return Err((StatusCode::BAD_REQUEST, String::from("Job does not exist or is not active")).into_response());
     };
 
@@ -79,12 +102,14 @@ pub struct JobsCompleteRequest {
     output_addr: Option<String>,
 }
 pub async fn complete(
+    State(state): State<AxumState>,
     Path(job_id): Path<Uuid>,
     Json(payload): Json<JobsCompleteRequest>,
 ) -> JsonHandlerResult<Value> {
+    let mut job_queue = state.job_queue.lock().await;
     let res = match payload.status {
-        JobCompletionStatus::Failed => fail_job(&job_id, &payload.output_addr),
-        JobCompletionStatus::Completed => complete_job(&job_id, &payload.output_addr),
+        JobCompletionStatus::Failed => job_queue.fail_job(&job_id, &payload.output_addr),
+        JobCompletionStatus::Completed => job_queue.complete_job(&job_id, &payload.output_addr),
     };
     let Ok(()) = res else {
         return Err((StatusCode::BAD_REQUEST, String::from("Job does not exist or is not active")).into_response());
@@ -93,8 +118,14 @@ pub async fn complete(
     Ok(Json(json!({})))
 }
 
-pub async fn get(Path(job_id): Path<Uuid>) -> JsonHandlerResult<Job> {
-    let Ok(job) = get_job(&job_id) else {
+pub async fn get(
+    Path(job_id): Path<Uuid>,
+    State(state): State<AxumState>,
+) -> JsonHandlerResult<Job> {
+    let mut job_queue = state.job_queue.lock().await;
+    job_queue.detect_abandoned_jobs();
+
+    let Ok(job) = job_queue.get_job(&job_id) else {
         return Err((StatusCode::NOT_FOUND, String::from("Job does not exist")).into_response());
     };
 
