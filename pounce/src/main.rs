1abd8da292af65807fda0f2d691e8de98bfee71f
Mark Christian <m@rkchristian.ca>
2022-12-16 10:39:24 -0800

    Refactor to optional read input from stdin, rather than optionally reading WASM binary from stdin

--- pounce/src/main.rs
+++ pounce/src/main.rs
@@ -36,10 +36,11 @@ pub enum Command {
         /// A description for the job
         #[clap(long, short)]
         description: Option<String>,
-        /// The file containing the wasm binary to run. Omit to read from stdin.
-        #[clap(value_name = "FILE")]
-        binary_file: Option<PathBuf>,
-        /// Path to a file to pass to the binary
+        /// The file containing the wasm binary to run
+        #[clap(value_name = "WASM BINARY")]
+        binary_file: PathBuf,
+        /// Path to a file to pass to the binary; omit to read from stdin (if present)
+        #[clap(value_name = "OPTIONAL INPUT TO WASM BINARY")]
         input_file: Option<PathBuf>,
     },
     /// Get the status of a job in progress.
@@ -65,41 +66,47 @@ fn build_url(path: String) -> String {
 }
 
 /// Convenience function to read an input wasm binary either from a pathbuf or from stdin.
-fn read_binary(maybepath: Option<PathBuf>) -> Result<Vec<u8>, anyhow::Error> {
+fn read_file_or_stdin(maybepath: Option<PathBuf>) -> Result<Vec<u8>, anyhow::Error> {
     // TODO This implementation should become a streaming implementation.
-    let mut binary: Vec<u8> = Vec::new();
-    let size = if let Some(ref fpath) = maybepath {
-        let file = File::open(fpath)?;
-        let mut reader = BufReader::new(file);
-        reader.read_to_end(&mut binary)?
-    } else {
-        let mut reader = BufReader::new(std::io::stdin());
-        reader.read_to_end(&mut binary)?
-    };
-
-    if size == 0 {
-        Err(anyhow!("no executable data read!"))
-    } else {
-        Ok(binary)
+    let mut buf: Vec<u8> = Vec::new();
+    if let Some(fpath) = maybepath {
+        return read_file(fpath);
     }
+
+    if atty::is(atty::Stream::Stdin) {
+        return Ok(buf);
+    }
+
+    let mut reader = BufReader::new(std::io::stdin());
+    reader.read_to_end(&mut buf)?;
+
+    Ok(buf)
+}
+
+fn read_file(path: PathBuf) -> Result<Vec<u8>, anyhow::Error> {
+    // TODO This implementation should become a streaming implementation.
+    let mut buf: Vec<u8> = Vec::new();
+    let file = File::open(path)?;
+    let mut reader = BufReader::new(file);
+    reader.read_to_end(&mut buf)?;
+
+    Ok(buf)
 }
 
 /// Post a wasm executable to a waiting agent to run.
 fn run(
     name: Option<String>,
     description: Option<String>,
-    maybebinarypath: Option<PathBuf>,
+    binarypath: PathBuf,
     maybeinputpath: Option<PathBuf>,
 ) -> Result<()> {
-    let binary = read_binary(maybebinarypath)?;
+    let binary = read_file(binarypath)?;
+    if binary.is_empty() {
+        return Err(anyhow!("no executable data read!"));
+    }
     let binary_part = reqwest::blocking::multipart::Part::bytes(binary);
 
-    let mut input_bytes: Vec<u8> = Vec::new();
-    if let Some(inputpath) = maybeinputpath {
-        let file = File::open(inputpath)?;
-        let mut reader = BufReader::new(file);
-        reader.read_to_end(&mut input_bytes)?;
-    };
+    let input_bytes = read_file_or_stdin(maybeinputpath)?;
     let input_part = reqwest::blocking::multipart::Part::bytes(input_bytes);
 
     let envelope = serde_json::json!({
