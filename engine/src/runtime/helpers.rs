8f7039dd1c9a2a9d9123e067fd559337cc529228
Mark Christian <m@rkchristian.ca>
2023-01-20 10:38:31 -0800

    Set up a structure for exposing functionality to our guest programs

---
+++ engine/src/runtime/helpers.rs
@@ -0,0 +1,99 @@
+use std::mem::size_of;
+
+use anyhow::anyhow;
+use wasmtime::{Caller, Extern, Memory, Val};
+
+///
+/// Calls into the guest environment to allocate a chunk of memory of the given size. if the guest
+/// does not expose a compatible alloc function, this will fail, and data exchange with the guest
+/// will not be possible. Our SDK automatically provides guest apps with this function.
+///
+pub fn alloc<T>(
+    caller: &mut Caller<'_, T>,
+    num_bytes_required: usize,
+) -> Result<usize, anyhow::Error> {
+    let Ok(alloc) = get_func_from_caller(caller, "alloc") else {
+        return Err(anyhow!("Failed to get alloc function from guest"));
+    };
+
+    // Note: We're casting an unsigned usize into an i32, which means we're losing
+    // half of our value range in wasm32. This shouldn't be a problem in practice,
+    // but I am calling it out here since it's not ideal. We can switch to using an
+    // I64 if it ever turns out to be a problem.
+    let params: Vec<Val> = vec![Val::I32(num_bytes_required as i32)];
+    let mut results: Vec<Val> = vec![Val::I32(0)];
+
+    alloc.call(caller, &params, &mut results)?;
+    let wasmtime::Val::I32(ptr) = results[0] else {
+        return Err(anyhow!("Alloc call failed"));
+    };
+    Ok(ptr as usize)
+}
+
+///
+/// Returns a handle to the exported guest function with the given name, or an error if none exists.
+///
+pub fn get_func_from_caller<T>(
+    caller: &mut Caller<'_, T>,
+    export_name: &str,
+) -> Result<wasmtime::Func, ()> {
+    match caller.get_export(export_name) {
+        Some(Extern::Func(f)) => Ok(f),
+        _ => Err(()),
+    }
+}
+
+///
+/// Returns a handle to the guest environment's Memory object.
+///
+pub fn get_memory_from_caller<T>(caller: &mut Caller<'_, T>) -> Result<Memory, ()> {
+    match caller.get_export("memory") {
+        Some(Extern::Memory(mem)) => Ok(mem),
+        _ => Err(()),
+    }
+}
+
+///
+/// Reads `len` bytes of data from the guest's memory starting at `ptr`.
+///
+pub fn read_bytes<T>(
+    caller: &Caller<'_, T>,
+    memory: Memory,
+    ptr: u32,
+    len: u32,
+) -> Result<Vec<u8>, anyhow::Error> {
+    let mut buf: Vec<u8> = vec![0; len as usize];
+    if let Err(err) = memory.read(caller, ptr.to_owned() as usize, &mut buf) {
+        eprintln!("Memory access error: {err:?}");
+        return Err(anyhow!(err));
+    };
+    Ok(buf)
+}
+
+///
+/// Writes the given data into the guest's memory, prefixed with a u32 indicating how many bytes of
+/// data were written. That is, if we want write the bytes [10, 20, 30, 40], this function will
+/// actually allocate 8 bytes total: 4 bytes for a u32 indicating the length of the data, followed by
+/// the 4 bytes of data itself. So, the value returned by this function would point to a chunk of
+/// memory containing the byte sequence [4, 0, 0, 0, 10, 20, 30, 40].
+/// A peer function to this one (to go from a pointer in shared memory to a Vec<u8> containing data)
+/// exists in the SDK as `get_bytes_from_host`.
+///
+pub fn write_bytes<T>(
+    caller: &mut Caller<'_, T>,
+    memory: &Memory,
+    bytes: Vec<u8>,
+) -> Result<usize, anyhow::Error> {
+    // Allocate enough memory to write a u32 + the contents of `bytes`. We'll write
+    // the length of bytes as a u32 at the start of the memory range, followed by
+    // the contents of `bytes`.
+    let num_bytes_required = size_of::<u32>() + bytes.len();
+    let ptr = alloc(caller, num_bytes_required)?;
+
+    // Now, copy the data over
+    let len_bytes = Vec::from((bytes.len() as u32).to_le_bytes());
+    let out_buffer = [len_bytes, bytes].concat();
+    memory.write(caller, ptr, &out_buffer)?;
+
+    Ok(ptr)
+}
