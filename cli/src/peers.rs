da6d78d74ba7c84830439830eabb923b3184a787
C J Silverio <ceejceej@gmail.com>
2023-04-06 12:26:06 -0700

    We don't care about roles any more in this particular search.

--- cli/src/peers.rs
+++ cli/src/peers.rs
@@ -11,7 +11,7 @@ static SERVAL_NODE_ADDR: OnceCell<SocketAddr> = async_once_cell::OnceCell::new()
 async fn base_url() -> SocketAddr {
     *SERVAL_NODE_ADDR
         .get_or_init(async {
-            maybe_find_peer(None, "SERVAL_NODE_URL")
+            maybe_find_peer("SERVAL_NODE_URL")
                 .await
                 .expect("unable to find any mesh peers!")
         })
@@ -48,18 +48,18 @@ pub async fn create_mesh_peer() -> Result<ServalMesh> {
     Ok(mesh)
 }
 
-async fn maybe_find_peer(role: Option<&ServalRole>, override_var: &str) -> Result<SocketAddr> {
+async fn maybe_find_peer(override_var: &str) -> Result<SocketAddr> {
     if let Ok(override_url) = std::env::var(override_var) {
         if let Ok(override_addr) = override_url.parse::<SocketAddr>() {
             return Ok(override_addr);
         }
     }
 
-    log::info!("Looking for {role:?} node on the peer network...");
+    log::info!("Looking for any node on the peer network...");
     let peer = discover_peer().await?;
     if let Some(addr) = peer.http_address() {
         Ok(addr)
     } else {
-        Err(anyhow!("Unable to locate a peer with the {role:?} role"))
+        Err(anyhow!("Unable to locate a peer"))
     }
 }
