7e9b772d485d99b733c91652b3b4b3fef40b6588
Mark Christian <mark@serval.sh>
2023-04-06 17:14:02 -0700

    Monitor: drain continuously while data is available, print out HTTP port if there is one

--- cli/src/mesh.rs
+++ cli/src/mesh.rs
@@ -16,13 +16,13 @@ pub async fn monitor_mesh() -> anyhow::Result<()> {
         .expect("Unable to get departures channel!");
 
     loop {
-        if let Some((addr, identity)) = discover_rx.recv().await {
+        while let Some((addr, identity)) = discover_rx.recv().await {
             let peer = PeerMetadata::from_identity(addr, identity.to_vec());
             if let Some(peer_address) = peer.address() {
                 print!(
                     "✅ {} {} @ {peer_address}",
                     "JOINED:".blue(),
-                    peer.instance_id()
+                    peer.instance_id(),
                 );
             } else {
                 print!(
@@ -31,16 +31,22 @@ pub async fn monitor_mesh() -> anyhow::Result<()> {
                     peer.instance_id()
                 );
             }
-            println!(
-                "; roles: {}",
-                peer.roles()
-                    .iter()
-                    .map(|xs| xs.to_string())
-                    .collect::<Vec<String>>()
-                    .join(", ")
-            );
+            if !peer.roles().is_empty() {
+                print!(
+                    "; roles: {}",
+                    peer.roles()
+                        .iter()
+                        .map(|xs| xs.to_string())
+                        .collect::<Vec<String>>()
+                        .join(", ")
+                );
+            }
+            if let Some(http_addr) = peer.http_address() {
+                print!("; http port: {}", http_addr.port());
+            }
+            println!();
         }
-        if let Some(addr) = depart_rx.recv().await {
+        while let Some(addr) = depart_rx.recv().await {
             println!("❌ {} {}", "DEPARTED:".red(), addr);
         }
         sleep(Duration::from_secs(1)).await;
