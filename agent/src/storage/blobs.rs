bf5224f68ee4679ce81a5c12cc811b632fc43525
C J Silverio <ceejceej@gmail.com>
2023-04-13 14:00:21 -0700

    Documentation for the storage traits & impls.
    I also cleaned up by removing everything that looked like
    we really wouldn't need it.

--- agent/src/storage/blobs.rs
+++ agent/src/storage/blobs.rs
@@ -13,6 +13,11 @@ use utils::structs::Manifest;
 
 use crate::storage::RunnerStorage;
 
+/// This struct manages an agent's local cache of wasm jobs (manifests and executables).
+/// This cache uses the cacache crate behind the scenes, but this is an implementation detail
+/// we've hidden here. There are three functions that are speculative implementations
+/// of some features that cacache allows us to have easily but that we have not yet exposed
+/// in the agent's API. Those are marked as lint exceptions.
 #[derive(Clone, Debug, Serialize)]
 pub struct BlobStore {
     location: PathBuf,
@@ -39,55 +44,6 @@ impl BlobStore {
         Ok(Self { location })
     }
 
-    /// Store a job type manifest. Returns the integrity checksum.
-    pub async fn store_manifest(&self, manifest: &Manifest) -> Result<Integrity, ServalError> {
-        let toml = toml::to_string(manifest)?;
-        let meta_sri = cacache::write(&self.location, manifest.manifest_key(), &toml).await?;
-        Ok(meta_sri)
-    }
-
-    /* Unused; consider removing
-        /// Store a job with metadata and an executable for later use. Returns the integrity checksums for the pair.
-        pub async fn store_manifest_and_executable(
-            &self,
-            manifest: &Manifest,
-            executable: &[u8],
-        ) -> Result<(Integrity, Integrity), ServalError> {
-            let toml = toml::to_string(manifest)?;
-            let meta_sri = cacache::write(&self.location, manifest.manifest_key(), &toml).await?;
-            let exec_sri =
-                cacache::write(&self.location, manifest.executable_key(), executable).await?;
-
-            Ok((meta_sri, exec_sri))
-        }
-    */
-
-    /// Store an executable in our blob store by its fully-qualified manifest name and a version string.
-    pub async fn store_executable(
-        &self,
-        name: &str,
-        version: &str,
-        bytes: &[u8],
-    ) -> Result<Integrity, ServalError> {
-        let key = Manifest::make_executable_key(name, version);
-        let sri = cacache::write(&self.location, key, bytes).await?;
-        Ok(sri)
-    }
-
-    /* Unused; consider removing
-        /// Given a content address, return a read stream for the object stored there.
-        /// Responds with an error if no object is found or if the address is invalid.
-        pub async fn executable_by_sri(
-            &self,
-            address: &str,
-        ) -> Result<ReaderStream<Reader>, ServalError> {
-            let integrity: Integrity = address.parse()?;
-            let fd = cacache::Reader::open_hash(&self.location, integrity).await?;
-            let stream = ReaderStream::new(fd);
-            Ok(stream)
-        }
-    */
-
     /// Fetch an executable by key as a read stream.
     pub async fn executable_as_stream(
         &self,
@@ -100,13 +56,27 @@ impl BlobStore {
         Ok(stream)
     }
 
-    /* Unused; consider removing
-        /// Checks if the given blob is in the content store, by its SRI string.
-        pub async fn data_exists_by_hash(&self, address: &str) -> Result<bool, ServalError> {
-            let integrity: Integrity = address.parse()?;
-            Ok(cacache::exists(&self.location, &integrity).await)
-        }
-    */
+    // Unused; consider removing
+    #[allow(dead_code)]
+    /// Given a content address, return a read stream for the object stored there.
+    /// Responds with an error if no object is found or if the address is invalid.
+    pub async fn executable_by_sri(
+        &self,
+        address: &str,
+    ) -> Result<ReaderStream<Reader>, ServalError> {
+        let integrity: Integrity = address.parse()?;
+        let fd = cacache::Reader::open_hash(&self.location, integrity).await?;
+        let stream = ReaderStream::new(fd);
+        Ok(stream)
+    }
+
+    // Unused; consider removing
+    #[allow(dead_code)]
+    /// Checks if the given blob is in the content store, by its SRI string.
+    pub async fn data_exists_by_hash(&self, address: &str) -> Result<bool, ServalError> {
+        let integrity: Integrity = address.parse()?;
+        Ok(cacache::exists(&self.location, &integrity).await)
+    }
 
     /// Checks if the given job type is present in our data store, using the fully-qualified name.
     pub async fn data_exists_by_key(&self, fq_name: &str) -> Result<bool, ServalError> {
@@ -116,21 +86,10 @@ impl BlobStore {
             Err(_) => Ok(false), // TODO: probably should handle errors more granularly
         }
     }
-
-    pub async fn manifest_names(&self) -> Result<Vec<String>, ServalError> {
-        let result: Vec<String> = cacache::list_sync(&self.location)
-            .filter(|xs| xs.is_ok())
-            .map(|xs| xs.unwrap().key)
-            .filter(|xs| xs.contains("manifest"))
-            .collect();
-        Ok(result)
-    }
 }
 
 #[async_trait]
 impl RunnerStorage for &BlobStore {
-    // type A = Reader;
-
     /// Fetch a manifest by its fully-qualified name.
     async fn manifest(&self, fq_name: &str) -> Result<Manifest, ServalError> {
         let bytes = cacache::read(&self.location, Manifest::make_manifest_key(fq_name)).await?;
@@ -143,10 +102,40 @@ impl RunnerStorage for &BlobStore {
         }
     }
 
-    /// A non-streaming way to retrieve a stored blob. Prefer executable_as_stream() if you can.
+    /// A non-streaming way to retrieve a stored compiled Wasm task. Prefer executable_as_stream() if you do not
+    /// need the executable bytes in memory.
     async fn executable_as_bytes(&self, name: &str, version: &str) -> Result<Vec<u8>, ServalError> {
         let key = Manifest::make_executable_key(name, version);
         let binary: Vec<u8> = cacache::read(&self.location, key).await?;
         Ok(binary)
+    }
+
+    /// Retrieve a list of all Wasm manifests stored on this node.
+    async fn manifest_names(&self) -> Result<Vec<String>, ServalError> {
+        let result: Vec<String> = cacache::list_sync(&self.location)
+            .filter(|xs| xs.is_ok())
+            .map(|xs| xs.unwrap().key)
+            .filter(|xs| xs.contains("manifest"))
+            .collect();
+        Ok(result)
+    }
+
+    /// Store a Wasm manifest. Returns the integrity checksum.
+    async fn store_manifest(&self, manifest: &Manifest) -> Result<Integrity, ServalError> {
+        let toml = toml::to_string(manifest)?;
+        let meta_sri = cacache::write(&self.location, manifest.manifest_key(), &toml).await?;
+        Ok(meta_sri)
+    }
+
+    /// Store an executable in our blob store by its fully-qualified manifest name and a version string.
+    async fn store_executable(
+        &self,
+        name: &str,
+        version: &str,
+        bytes: &[u8],
+    ) -> Result<Integrity, ServalError> {
+        let key = Manifest::make_executable_key(name, version);
+        let sri = cacache::write(&self.location, key, bytes).await?;
+        Ok(sri)
     }
 }
--- agent/src/storage/proxy.rs
+++ agent/src/storage/proxy.rs
@@ -4,6 +4,7 @@ use std::net::SocketAddr;
 use async_trait::async_trait;
 
 use serval_client::ServalApiClient;
+use ssri::Integrity;
 use utils::errors::ServalError;
 use utils::structs::Manifest;
 
@@ -15,68 +16,12 @@ pub struct StorageProxy {
 }
 
 impl StorageProxy {
+    /// Create a new proxy to a peer at the specified address, using the requested API version.
     pub fn new(version: u8, address: SocketAddr) -> Self {
         Self {
             client: ServalApiClient::new_with_version(version, address.to_string()),
         }
     }
-    /*
-
-    pub async fn store_manifest(&self, manifest: &Manifest) -> Result<Integrity, ServalError> {
-        let integrity = self.client.store_manifest(manifest).await?;
-        Ok(integrity)
-    }
-
-    pub async fn store_manifest_and_executable(
-        &self,
-        manifest: &Manifest,
-        executable: &[u8],
-    ) -> Result<(Integrity, Integrity), ServalError> {
-        let m_integrity = self.client.store_manifest(manifest).await?;
-        let e_integrity = self
-            .client
-            .store_executable(&manifest.fq_name(), manifest.version(), executable.to_vec())
-            .await?;
-
-        Ok((m_integrity, e_integrity))
-    }
-
-    pub async fn store_executable(
-        &self,
-        name: &str,
-        version: &str,
-        bytes: &[u8],
-    ) -> Result<Integrity, ServalError> {
-        self.client
-            .store_executable(name, version, bytes.to_vec())
-            .await
-    }
-
-    pub async fn executable_by_sri(
-        &self,
-        _address: &str,
-    ) -> Result<Pin<Box<dyn AsyncRead>>, ServalError> {
-        // TODO needs to be exposed in the api first
-        // This function is NOT USED in api implementation. (consider removing)
-        todo!()
-    }
-
-    pub async fn data_exists_by_hash(&self, _address: &str) -> Result<bool, ServalError> {
-        // This needs to be exposed in an API to be proxied
-        // This function is NOT USED in api implementation. (consider removing)
-        todo!()
-    }
-
-    pub async fn data_exists_by_key(&self, _fq_name: &str) -> Result<bool, ServalError> {
-        // This needs to be exposed in an API to be proxied
-        // This function IS USED in api implementation.
-        todo!()
-    }
-
-    pub async fn manifest_names(&self) -> Result<Vec<String>, ServalError> {
-        self.client.list_manifests().await
-    }
-    */
 }
 
 #[async_trait]
@@ -87,5 +32,29 @@ impl RunnerStorage for StorageProxy {
 
     async fn executable_as_bytes(&self, name: &str, version: &str) -> Result<Vec<u8>, ServalError> {
         self.client.get_executable(name, version).await
+    }
+
+    // The following functions are speculative implementations of reaching out
+    // to a peer for data we do not have locally. We don't use any of these at the
+    // moment, but we might need to.
+
+    async fn store_manifest(&self, manifest: &Manifest) -> Result<Integrity, ServalError> {
+        let integrity = self.client.store_manifest(manifest).await?;
+        Ok(integrity)
+    }
+
+    async fn store_executable(
+        &self,
+        name: &str,
+        version: &str,
+        bytes: &[u8],
+    ) -> Result<Integrity, ServalError> {
+        self.client
+            .store_executable(name, version, bytes.to_vec())
+            .await
+    }
+
+    async fn manifest_names(&self) -> Result<Vec<String>, ServalError> {
+        self.client.list_manifests().await
     }
 }
