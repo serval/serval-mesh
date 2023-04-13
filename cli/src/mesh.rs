f3075efff4fa937891d7621d67f8e1fb8efce8fc
Mark Christian <mark@serval.sh>
2023-04-13 13:42:44 -0700

    serval monitor - press enter to print out all known peer latencies

--- cli/src/mesh.rs
+++ cli/src/mesh.rs
@@ -1,12 +1,17 @@
 use owo_colors::OwoColorize;
 use tokio::time::sleep;
 
-use std::time::Duration;
+use std::{
+    io::{stdin, Read, Write},
+    sync::mpsc::{self, Receiver},
+    time::Duration,
+};
 
 use utils::mesh::{KaboodlePeer, PeerMetadata};
 
 pub async fn monitor_mesh() -> anyhow::Result<()> {
     println!("Monitoring mesh ...");
+    let stdin_rx = spawn_stdin_reader();
     let mut mesh = super::peers::create_mesh_peer().await?;
     let mut discover_rx = mesh
         .discover_peers()
@@ -37,7 +42,38 @@ pub async fn monitor_mesh() -> anyhow::Result<()> {
         while let Ok(addr) = depart_rx.try_recv() {
             println!("❌ {} {}", "DEPARTED:".red(), addr);
         }
+        if stdin_rx.try_recv().is_ok() {
+            let latencies = mesh.peer_latencies().await;
+            if !latencies.is_empty() {
+                println!("{}", "LATENCIES:".blue());
+            }
+            for (peer, latency) in latencies.into_iter() {
+                let latency = format!("{:.2} ms", latency.as_micros() as f64 / 1000.0);
+                println!(
+                    "⏲️  {latency} to {} @ {}",
+                    peer.instance_id(),
+                    peer.address(),
+                );
+            }
+        }
         sleep(Duration::from_secs(1)).await;
     }
     // on ctrl-C, clean up?
+}
+
+fn spawn_stdin_reader() -> Receiver<()> {
+    let (tx, rx) = mpsc::channel::<()>();
+    std::thread::spawn(move || {
+        let mut character = [0];
+        loop {
+            while stdin().read(&mut character).is_err() {
+                // loop until we actually read something
+            }
+
+            std::io::stdout().flush().unwrap();
+            let _ = tx.send(());
+        }
+    });
+
+    rx
 }
